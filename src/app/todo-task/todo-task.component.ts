ae35303a75d4f2fa1e32b8cc67163698c80959e5
Faria Hasan Talukder <faria1136@gmail.com>
2024-12-15 13:41:30 +0000

    Add todo task, list, form, header, footer component

---
+++ src/app/todo-task/todo-task.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-task',
+  imports: [],
+  templateUrl: './todo-task.component.html',
+  styleUrl: './todo-task.component.css'
+})
+export class TodoTaskComponent {
+
+}
