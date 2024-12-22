e9aa5742f336cec491196e21f0f045329ee5d2b0
Faria Hasan Talukder <faria1136@gmail.com>
2024-12-22 20:18:58 +0000

    Added todo form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';  
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoFormComponent } from './ui/todo-form/todo-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, TodoListComponent, TodoFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
